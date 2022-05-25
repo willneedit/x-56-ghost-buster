05178d3a57e7a16581bf1528c58af43f2ffee409
Will Needit <willneedit@nufuerspam.de>
2022-05-25 16:09:13 +0200

    Implemented axes support and axis dropout

--- x56_debounce.py
+++ x56_debounce.py
@@ -24,25 +24,35 @@ for i in range(js.get_count()):
     joys[i] = js.Joystick(i)
     joys[i].init()
     
-    if joys[i].get_name() == "X56 H.O.T.A.S. Throttle":
+    if joys[i].get_name() == "Saitek Pro Flight X-56 Rhino Throttle":
         print("X56 Throttle found at index %d" % i)
         x56 = joys[i]
-        print("Buttons: %d\n" % x56.get_numbuttons())
+        print("Buttons: %d" % x56.get_numbuttons())
+        print("Axes: %d\n" % x56.get_numaxes())
         x56_found = True
     
-    if joys[i].get_name() == "vJoy Device":
+    elif joys[i].get_name() == "vJoy Device":
         print("vJoy Device found at index %d" % i)
         vJoy = joys[i]
-        print("Buttons: %d\n" % vJoy.get_numbuttons())
+        print("Buttons: %d" % vJoy.get_numbuttons())
+        print("Axes: %d\n" % vJoy.get_numaxes())
         vJoy_found = True
         j = vj.VJoyDevice(1)
 
+    else:
+        print("Other joystick/gamepad: %s\n" % joys[i].get_name())
+        
     if vJoy_found and x56_found:
         break
+        
 button_count = vJoy.get_numbuttons()
 button_timers = [-1] * button_count
 button_states = np.zeros(button_count)
 button_set = np.zeros(button_count)
+
+axis_count = vJoy.get_numaxes()
+axis_state = np.zeros(axis_count)
+axis_dropped = [False] * axis_count
 
 print("\nLooping...")
 
@@ -59,6 +69,21 @@ while  vJoy_found and x56_found:
                 if not x56.get_button(i):
                     button_timers[i] = -1.0
                     # print("Button %d up" % (i+1))
+
+    for i in range(axis_count):
+        new_axis_state = x56.get_axis(i)
+        axis_diff = new_axis_state - axis_state[i]
+        if axis_state[i] != new_axis_state:
+            if new_axis_state > 0.995 and axis_state[i] < 0.800:
+                print("Dropout in axis %d (%6.3f)!" % (i, new_axis_state))
+                old_axis_dropped = axis_dropped[i]
+                axis_dropped[i] = True
+                if old_axis_dropped:
+                    axis_state[i] = new_axis_state
+            else:
+                axis_dropped[i] = False
+                axis_state[i] = new_axis_state
+                # print("Axis %d state %6.3f" % (i , axis_state[i]))
 
     button_states_mem = np.copy(button_states)
 
@@ -79,6 +104,9 @@ while  vJoy_found and x56_found:
     if np.sum(button_states[button_count-3:button_count]) > 1:
         button_states[button_count-3:button_count] = np.copy(button_states_mem[button_count-3:button_count])
 
+    for i in range(axis_count):
+        j.set_axis(i + 0x30,int(axis_state[i] * 0x8000 + 0x3FFF))
+        
     for i in range(button_count):
         if (i>30 or i<29):
             if button_states[i] == 1 and button_set[i] == 0:
@@ -108,7 +136,7 @@ while  vJoy_found and x56_found:
         joys[i] = js.Joystick(i)
         joys[i].init()
         
-        if joys[i].get_name() == "X56 H.O.T.A.S. Throttle":
+        if joys[i].get_name() == "Saitek Pro Flight X-56 Rhino Throttle":
             x56_found = True
             x56 = joys[i]
         
